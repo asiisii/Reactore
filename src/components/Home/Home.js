5f8c5326731f01c916e2bffaaf86e20203678ece
Ashish Malla <ashishmalla45@gmail.com>
2021-06-26 21:16:02 -0600

    Add nav bar and a h1 tag

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,2 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { fetchProducts } from '../../apiData/apiCalls'
+import { Navbar } from '../Navbar/Navbar'
+export const Home = () => {
 
+
+  // useEffect(() => {
+  //   fetchProducts()
+  //     .then(data => console.log(data))
+  //   // console.log(products);
+  // }, [])
+
+  return (
+    <div className='Home'>
+      <Navbar />
+      <h1>Navbar</h1>
+    </div>
+  )
+}
