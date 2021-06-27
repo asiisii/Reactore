db3e6c89d9e59fe833b66c9e638e6e40ac7ba129
Ashish Malla <ashishmalla45@gmail.com>
2021-06-26 18:00:34 -0600

    Add route to home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Home from './components/Home/Home'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Switch>
+        <Route 
+          exact path='/'
+          component= { Home }
+        />
+      </Switch>
     </div>
   );
 }
