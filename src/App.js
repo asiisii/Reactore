96e62122389b07288c09f94572f98423f4b2ef17
Ashish Malla <ashishmalla45@gmail.com>
2021-06-26 21:15:27 -0600

    Connect Home with App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import Home from './components/Home/Home'
+import { Home } from './components/Home/Home'
 import './App.css';
 
 function App() {
