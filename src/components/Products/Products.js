c639b247ddebe9099bbc4897ca192f3b190976fb
Ashish Malla <ashishmalla45@gmail.com>
2021-06-26 23:55:21 -0600

    Update Card props

---
+++ src/components/Products/Products.js
@@ -0,0 +1,24 @@
+import React from 'react' 
+import { useSelector, useDispatch } from 'react-redux'
+import Card from '../Card/Card'
+
+const Products = () => {
+  const productsData = useSelector(store => store.productsData)
+  const productCards = productsData.map(product => {
+    return <Card 
+      key={product.id}
+      title={product.title}
+      description={product.description}
+      price={product.price}
+      category={product.category}
+      image={product.image}
+    />
+  })
+  return (
+    <div className='products-container'>
+   {productCards}
+    </div>
+  )
+}
+
+export default Products
