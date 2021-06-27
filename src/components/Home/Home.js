ea0272ab6fa8248f66dcf8b5b8c460e89392035f
Ashish Malla <ashishmalla45@gmail.com>
2021-06-27 04:16:08 -0600

    Import Form component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,11 @@ import React, { useEffect } from 'react'
 import { fetchProducts } from '../../apiData/apiCalls'
 import { Navbar } from '../Navbar/Navbar'
 import { useSelector, useDispatch } from 'react-redux'
-import storeProducts from '../../redux/actions'
+import {storeProducts} from '../../redux/actions'
 import Products from '../Products/Products'
-
+import Form from '../Form/Form'
 
 export const Home = () => {
-
   const dispatch = useDispatch()
   const productsData = useSelector(store => store.productsData)
 
@@ -25,6 +24,7 @@ export const Home = () => {
   return (
     <div className='Home'>
       <Navbar />
+      <Form />
       <div className="wrapper">
         {!productsData.length ? 
           <h1>Loading...</h1>  : 
