02c9965f0d9743f78368b1ce18dcc987941daad9
Ashish Malla <ashishmalla45@gmail.com>
2021-06-26 22:15:15 -0600

    Use dummy data instead of actual fetch call

--- src/apiData/apiCalls.js
+++ src/apiData/apiCalls.js
@@ -1,6 +1,5 @@
 const path = 'https://fakestoreapi.com/products'
 
 export const fetchProducts = async() => {
-  const response = await fetch(path)
-  return response.json()
+  return await fetch('data.json')
 }
