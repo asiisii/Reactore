b58b483f3e28d4436d6276d7ccc429438356c3bd
Ashish Malla <ashishmalla45@gmail.com>
2021-06-26 18:18:13 -0600

    Add ga global store

--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
+import allReducers from './redux/reducers/allReducers'
+// to create store we need to import createStore from redux
+import { createStore } from 'redux'
+// import Provider to connects our Global state called store to entire app
+import { Provider } from 'react-redux';
+
+const store = createStore(
+  allReducers,
+  // this will allow us to use Redux devetools on the browser
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
   <Router>
-    <App />
+   <Provider store={store}>
+      <App />
+    </Provider>
   </Router>,
   document.getElementById('root')
-);
+)
 
 
